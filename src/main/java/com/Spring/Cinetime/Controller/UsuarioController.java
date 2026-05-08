e975a7a6f33c80d2f3d48afbcb53d4e82d287e9f
Matheus Bressani <80131940+BressaniEND@users.noreply.github.com>
2026-05-08 20:39:15 -0300

    Add files via upload

---
+++ src/main/java/com/Spring/Cinetime/Controller/UsuarioController.java
@@ -0,0 +1,62 @@
+package br.com.Cinetime.Cinetime.Controller;
+
+import br.com.Cinetime.Cinetime.Dto.LoginDTO;
+import br.com.Cinetime.Cinetime.Dto.UsuarioRequestDTO;
+import br.com.Cinetime.Cinetime.Model.Usuario;
+import br.com.Cinetime.Cinetime.Service.UsuarioService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/Usuarios")
+@CrossOrigin("*")
+
+public class UsuarioController {
+
+    private final UsuarioService service;
+
+        public UsuarioController(UsuarioService service) {
+            this.service = service;
+        }
+
+        @GetMapping
+        public ResponseEntity<List<Usuario>> listAll()   {
+            return ResponseEntity.ok(service.findAll());
+        }
+
+        @GetMapping("/{id}")
+        public ResponseEntity<Usuario> findById(@PathVariable Long id) {
+            return ResponseEntity.ok(service.findById(id));
+        }
+
+        @PostMapping("/login")
+        public ResponseEntity<Usuario> Logar(@RequestBody @Valid LoginDTO dto){
+            return ResponseEntity.ok(service.login(dto));
+        }
+
+
+        @PostMapping("/cadastro")
+        public ResponseEntity<Usuario> create(@Valid @RequestBody UsuarioRequestDTO dto) {
+            return ResponseEntity.status(HttpStatus.CREATED)
+                    .body(service.createUser(dto));
+        }
+
+        @PutMapping("/{id}")
+        public ResponseEntity<Usuario> update(@PathVariable Long id, @Valid @RequestBody UsuarioRequestDTO dto) {
+
+            return ResponseEntity.ok(service.update(id, dto));
+        }
+
+        @DeleteMapping("/{id}")
+        public ResponseEntity<Void> delete(@PathVariable Long id) {
+            service.delete(id);
+            return ResponseEntity.noContent().build();
+        }
+
+
+    }
+
